b1beca352c09450b4e82899f0398f3e297f03195
mkbal <matthew.ballarino@my.jcu.edu.au>
2020-08-23 11:03:45 +1000

    "Refactor password check program to use functions"

--- prac_3/password_check.py
+++ prac_3/password_check.py
@@ -8,13 +8,29 @@ Student name: Matthew Ballarino
 Student number: 13291475
 """
 
-MINIMUM_LENGTH = 6
-
-password_input = input("Enter password that at least has {0} characters:".format(MINIMUM_LENGTH))
-
-while len(password_input) < MINIMUM_LENGTH:
-    password_input = input("Please enter password that at least has {0} characters:".format(MINIMUM_LENGTH))
-print('*' * len(password_input))
+import random
+LIST_RANDOM = [4, 5, 6, 8]
+MINIMUM_LENGTH = random.choice(LIST_RANDOM)
 
 
+def main():
+    """Getting set up"""
+    password = get_password(MINIMUM_LENGTH)
+    print_password(password)
 
+
+
+def get_password(length):
+    """Getting user input and making sure it meets requriments"""
+    password_input = input("Enter password that at least has {0} characters:".format(length))
+    while len(password_input) < length:
+        password_input = input("Please enter the correct amount of characters that least has {0} characters:".format(length))
+    return password_input
+
+
+def print_password(password_input):
+    """Printing user input for password in asterisks"""
+    print('*' * len(password_input))
+
+
+main()
