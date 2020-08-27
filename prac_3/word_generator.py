48ec117264c483b480f87e05c7676ac89d77f13c
mkbal <matthew.ballarino@my.jcu.edu.au>
2020-08-27 20:52:11 +1000

    "Creating a program that can tell which ascii is what for both numbers and letters included a directory and columns"

--- prac_3/word_generator.py
+++ prac_3/word_generator.py
@@ -9,31 +9,52 @@ Student number: 13291475
 """
 import random
 
-user_input = int(input("How many characters is in the password?: "))
-
-
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 SPECIAL_CHARCTERS = '%$#@*&'
 CAPTIAL_CONSONANTS = "BCDFGHJKLMNPRQRSTVWXYZ"
 CAPTIAL_VOWELS = "AEIOU"
 
-guessing = "cvdrh"
-word_format = "".join(random.choice(guessing)for i in range(user_input))
 
-print(word_format)
+def main():
+    guessing = "cvdrh"
+    user_input = 0
+    try:
+        user_input = int(input("How many characters is in the password?: "))
+        while user_input == 0:
+            user_input = int("No zero please try again")
 
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    elif kind == "d":
-         word += random.choice(SPECIAL_CHARCTERS)
-    elif kind == 'r':
-         word += random.choice(CAPTIAL_CONSONANTS)
-    elif kind == 'h':
-        word += random.choice(CAPTIAL_VOWELS)
-    else:
-        word += random.choice(VOWELS)
+    except ValueError:
+            print("Please enter how characters their are in numberical form")
 
-print("The password is {0} with a string length of {1}".format(word, user_input))
+    word_format = "".join(random.choice(guessing) for i in range(user_input))
+
+    print(word_format)
+
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        elif kind == "d":
+            word += random.choice(SPECIAL_CHARCTERS)
+        elif kind == 'r':
+            word += random.choice(CAPTIAL_CONSONANTS)
+        elif kind == 'h':
+            word += random.choice(CAPTIAL_VOWELS)
+        else:
+            word += random.choice(VOWELS)
+
+    print("The password is {0} with a string length of {1}".format(word, user_input))
+
+main()
+# finished = False
+# result = 0
+# while not finished:
+#     try:
+#          result = int(input("Enter a number: "))
+#          finished= True
+#          # the true function ends the loop
+#     except:
+#             print("Please enter a valid integer.")
+#             result = int(input("Please enter a valid number: "))
+# print("Valid result is:", result)
