5f23269ae68a035393ead1559faa68cfd6d02bf7
mkbal <matthew.ballarino@my.jcu.edu.au>
2020-09-10 19:12:11 +1000

    "recreated a program that can turn state colour code into number code of colour"

--- prac_5/state_names.py
+++ prac_5/state_names.py
@@ -4,13 +4,13 @@ State names in a dictionary
 File needs reformatting
 """
 
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia", "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia", "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(STATE_NAMES)
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+    if state_code in STATE_NAMES:
+        print(state_code, "is", STATE_NAMES[state_code])
 
     else:
         print("Invalid short state")
