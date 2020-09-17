84fda4bdc2f083c53f88a3ae6974cc1ecb981496
mkbal <matthew.ballarino@my.jcu.edu.au>
2020-09-17 22:31:52 +1000

    "Making our own simple class for a programming language"

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -7,10 +7,28 @@ Student name: Matthew Ballarino
 Student number: 13291475
 """
 
+class Programing_Language:
+    """Information about programing language"""
 
 
+    def __init__(self, name, typing, reflection, year):
+        """Creating a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+    def __str__(self):
+        """Return string representation of a ProgrammingLanguage."""
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(
+            self.name, self.typing, self.reflection, self.year)
 
 
+    def is_dynamic(self):
+        """Determine if language is dynamically typed."""
+        return self.typing == "Dynamic"
+
+
+if __name__ == "__main__":
 
 
 
